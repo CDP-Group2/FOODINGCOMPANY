d3170078ecc36bb53522338f412a902fc6a8deab
jaemyungskku <jaemyung@skku.edu>
2017-11-08 16:21:18 +0900

    add QR gallery save

--- app/src/main/java/com/fooding/companyapp/activity/SendOutQRActivity.java
+++ app/src/main/java/com/fooding/companyapp/activity/SendOutQRActivity.java
@@ -7,10 +7,12 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.fooding.companyapp.HttpConnection;
@@ -22,7 +24,12 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,6 +45,65 @@ public class SendOutQRActivity extends AppCompatActivity {
     private Bitmap bitmap=null;
     @BindView(R.id.QRView) ImageView QRimage;
     @OnClick(R.id.share_button) void QRcode_Share() {
+        if(bitmap!=null) {
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("image/png");
+            share.addCategory(Intent.CATEGORY_DEFAULT);
+            String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                    bitmap, "image", null);
+            Uri imageUri =  Uri.parse(path);
+            share.putExtra(Intent.EXTRA_STREAM, imageUri);
+            startActivity(Intent.createChooser(share, "Share"));
+        }
+    }
+    @OnClick(R.id.save_button) void QRcode_Save() {
+        try {
+            if (bitmap != null) {
+                FileOutputStream stream;
+                long now;
+                Date date;
+                now = System.currentTimeMillis();
+                date = new Date(now);
+                SimpleDateFormat sdfnow = new SimpleDateFormat("yyyyMMddHHmmss");
+                String strnow = sdfnow.format(date);
+                String folder = Environment.getExternalStorageDirectory().getAbsolutePath()
+                     + "/DCIM/FOODING";
+                String path = folder + File.separator + strnow + ".png";
+                File FolderPath = new File(folder);
+                if (!FolderPath.exists()) {
+                    FolderPath.mkdirs();
+                    Log.d("MKDIR", folder);
+                }
+                stream = new FileOutputStream(path);
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+                stream.close();
+                sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://"+path)));
+                Toast.makeText(this,"Saved.",Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_out_qr);
+        ButterKnife.bind(this);
+        //intent로 원문 받아오기 - food로 받아와도 됨
+        String codeString = getIntent().getStringExtra("Code");
+        /////////
+        //writer 셋업
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        try {
+            //QRcode 생성
+            BitMatrix bitMatrix = barcodeWriter.encode(codeString, BarcodeFormat.QR_CODE,1000,1000);
+            BarcodeEncoder encoder = new BarcodeEncoder();
+            bitmap = encoder.createBitmap(bitMatrix);
+            QRimage.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 // Should we show an explanation?
@@ -55,40 +121,6 @@ public class SendOutQRActivity extends AppCompatActivity {
                     // result of the request.
                 }
             }
-            else sharebitmap();
-        }
-        else sharebitmap();
-    }
-    private void sharebitmap(){
-        if(bitmap!=null) {
-            Intent share = new Intent(Intent.ACTION_SEND);
-            share.setType("image/png");
-            share.addCategory(Intent.CATEGORY_DEFAULT);
-            String path = MediaStore.Images.Media.insertImage(getContentResolver(),
-                    bitmap, "image", null);
-            Uri imageUri =  Uri.parse(path);
-            share.putExtra(Intent.EXTRA_STREAM, imageUri);
-            startActivity(Intent.createChooser(share, "Share"));
-        }
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_out_qr);
-        ButterKnife.bind(this);
-        //intent로 원문 받아오기 - food로 받아와도 됨
-        String codeString = getIntent().getStringExtra("Code");
-        /////////
-        //writer 셋업
-        QRCodeWriter barcodeWriter = new QRCodeWriter();
-        try {
-            //QRcode 생성
-            BitMatrix bitMatrix = barcodeWriter.encode(codeString, BarcodeFormat.QR_CODE,350,350);
-            BarcodeEncoder encoder = new BarcodeEncoder();
-            bitmap = encoder.createBitmap(bitMatrix);
-            QRimage.setImageBitmap(bitmap);
-        } catch (WriterException e) {
-            e.printStackTrace();
         }
     }
 
@@ -120,8 +152,7 @@ public class SendOutQRActivity extends AppCompatActivity {
                // If request is cancelled, the result arrays are empty.
                if (grantResults.length > 0
                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                   sharebitmap();
+                   // permission granted
                } else {
                    // permission denied, boo! Disable the
                    // functionality that depends on this permission.
