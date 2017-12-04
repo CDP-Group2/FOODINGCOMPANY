ec4a22a1875f815926c8a0dcf7a4dfc53516f4ae
hansanghoon <s2peters2@naver.com>
2017-12-04 11:25:53 +0900

    set ingredientamount add

--- app/src/main/java/com/fooding/companyapp/activity/MakeRecipeActivity.java
+++ app/src/main/java/com/fooding/companyapp/activity/MakeRecipeActivity.java
@@ -1,14 +1,19 @@
 package com.fooding.companyapp.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -30,19 +35,23 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import com.travijuu.numberpicker.library.NumberPicker;
 
-public class MakeRecipeActivity extends AppCompatActivity {
+public class MakeRecipeActivity extends AppCompatActivity{
     @BindView(R.id.addButton) Button addBtn;
     @BindView(R.id.deleteButton) Button deleteBtn;
     @BindView(R.id.makeButton) Button makeBtn;
     @BindView(R.id.ingredientList) ListView ingredientList;
     @BindView(R.id.recipeName) EditText recipeNameText;
     final Integer ADD_INGREDIENT = 1;
+    final Integer SET_AMOUNT = 2;
     public ArrayList<String> ingredients;
     public ArrayList<String> ingredientsID;
+    public ArrayList<Integer> ingredientsAmount;
     public ArrayAdapter adapter;
     private Food food;
     private FoodingCompanyApplication app;
+    private Boolean amountFlag=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +59,12 @@ public class MakeRecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_make_recipe);
         ButterKnife.bind(this);
 
+        amountFlag=false;
+
+
         ingredients = new ArrayList<String>();
         ingredientsID = new ArrayList<String>();
+        ingredientsAmount = new ArrayList<Integer>();
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, ingredients) ;
         ingredientList.setAdapter(adapter);
 
@@ -79,6 +92,24 @@ public class MakeRecipeActivity extends AppCompatActivity {
             }
         });
 
+        ingredientList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(amountFlag){
+                    Intent intent = new Intent(MakeRecipeActivity.this, amountPopupActivity.class);
+                    intent.putExtra("ingredientName",ingredients.get(position).toString());
+                    intent.putExtra("ingredientPS",Integer.toString(position));
+                    intent.putExtra("ingredientAmount",ingredientsAmount.get(position).toString());
+
+                    Log.i("name",ingredients.get(position).toString());
+                    Log.i("id",ingredientsID.get(position).toString());
+                    Log.i("amoutn",ingredientsAmount.get(position).toString());
+
+                    startActivityForResult(intent,SET_AMOUNT);
+                }
+            }
+        });
+
         makeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -96,13 +127,9 @@ public class MakeRecipeActivity extends AppCompatActivity {
 
                 app.setCurrentFood(food);
 
-                Retrofit retrofit;
-                APIService apiService;
 
-                retrofit = new Retrofit.Builder().baseUrl(APIService.API_URL).build();
-                apiService = retrofit.create(APIService.class);
 
-                ArrayList<String> tmp = new ArrayList<String>();
+                final ArrayList<String> tmp = new ArrayList<String>();
                 food = app.getCurrentFood();
                 Map<String, String> ingredients= food.getIngredient();
                 Iterator<String> iterator = ingredients.keySet().iterator();
@@ -113,32 +140,87 @@ public class MakeRecipeActivity extends AppCompatActivity {
                 }
 
                 Log.i("lenof tmp", Integer.toString(tmp.size()));
-
-                Call<ResponseBody> comment = apiService.makeRecipe("1", recipeNameText.getText().toString(), tmp);
-                comment.enqueue(new Callback<ResponseBody>() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MakeRecipeActivity.this);
+                builder.setCancelable(true);
+                builder.setMessage("영양정보를 추가하시겠습니까?");
+                builder.setPositiveButton("NO", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        try{
-                            String temp = response.body().string();
-                            Food food = FoodingCompanyApplication.getInstance().getCurrentFood();
-                            Log.i("string temp",temp);
-                            food.setName(temp);
-                            FoodingCompanyApplication.getInstance().setCurrentFood(food);
-                        } catch(IOException e){
-                            Log.i("Test1", "fail");
-                            e.printStackTrace();
-                        }
+                    public void onClick(DialogInterface dialog, int which) {
+                        Retrofit retrofit;
+                        APIService apiService;
+
+                        retrofit = new Retrofit.Builder().baseUrl(APIService.API_URL).build();
+                        apiService = retrofit.create(APIService.class);
+                        Call<ResponseBody> comment = apiService.makeRecipe("1", recipeNameText.getText().toString(), tmp);
+                        comment.enqueue(new Callback<ResponseBody>() {
+                            @Override
+                            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                                try{
+                                    String temp = response.body().string();
+                                    Food food = FoodingCompanyApplication.getInstance().getCurrentFood();
+                                    Log.i("string temp",temp);
+                                    food.setName(temp);
+                                    FoodingCompanyApplication.getInstance().setCurrentFood(food);
+                                } catch(IOException e){
+                                    Log.i("Test1", "fail");
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                                Log.i("Test1", "onfailure");
+                                t.printStackTrace();
+                            }
+                        });
+
+                        startActivity(new Intent(MakeRecipeActivity.this, ViewRecipeActivity.class));
+                        finish();
                     }
-
+                });
+                builder.setNegativeButton("YES", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        Log.i("Test1", "onfailure");
-                        t.printStackTrace();
+                    public void onClick(DialogInterface dialog, int which) {
+                        amountFlag=true;
                     }
                 });
 
-                startActivity(new Intent(MakeRecipeActivity.this, ViewRecipeActivity.class));
-                finish();
+                if(amountFlag){
+                    Retrofit retrofit;
+                    APIService apiService;
+
+                    retrofit = new Retrofit.Builder().baseUrl(APIService.API_URL).build();
+                    apiService = retrofit.create(APIService.class);
+                    Call<ResponseBody> comment = apiService.makeRecipe("1", recipeNameText.getText().toString(), tmp);
+                    comment.enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            try{
+                                String temp = response.body().string();
+                                Food food = FoodingCompanyApplication.getInstance().getCurrentFood();
+                                Log.i("string temp",temp);
+                                food.setName(temp);
+                                FoodingCompanyApplication.getInstance().setCurrentFood(food);
+                            } catch(IOException e){
+                                Log.i("Test1", "fail");
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            Log.i("Test1", "onfailure");
+                            t.printStackTrace();
+                        }
+                    });
+
+                    startActivity(new Intent(MakeRecipeActivity.this, ViewRecipeActivity.class));
+                    finish();
+                }
+                else{
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
             }
         });
     }
@@ -146,16 +228,25 @@ public class MakeRecipeActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == 1) {
+        if (requestCode == ADD_INGREDIENT) {
             if(resultCode == Activity.RESULT_OK){
                 String addIngredient=data.getStringExtra("addIngredient");
                 String addIngredientID=data.getStringExtra("addIngredientID");
                 ingredients.add(addIngredient);
                 ingredientsID.add(addIngredientID);
+                ingredientsAmount.add(10);
                 adapter.notifyDataSetChanged();
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 //만약 반환값이 없을 경우의 코드를 여기에 작성하세요.
+            }
+        }
+
+        if(requestCode == SET_AMOUNT){
+            if(resultCode==Activity.RESULT_OK){
+                Integer setIngredient = Integer.parseInt(data.getStringExtra("setIngredient"));
+                Integer setIngredientAmount = Integer.parseInt(data.getStringExtra("setIngredientAmount"));
+                ingredientsAmount.set(setIngredient,setIngredientAmount);
             }
         }
     }//onActivityResult
---
+++ app/src/main/java/com/fooding/companyapp/activity/amountPopupActivity.java
@@ -0,0 +1,103 @@
+package com.fooding.companyapp.activity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fooding.companyapp.APIService;
+import com.fooding.companyapp.FoodingCompanyApplication;
+import com.fooding.companyapp.R;
+import com.fooding.companyapp.data.Food;
+import com.fooding.companyapp.data.model.Ingredient;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class amountPopupActivity extends Activity {
+    @BindView(R.id.okay) Button ok;
+    @BindView(R.id.minus) Button minus;
+    @BindView(R.id.plus) Button plus;
+    @BindView(R.id.amountGram) EditText amountGram;
+    @BindView(R.id.ingredientName) TextView ingreName;
+
+    private String ingredientName;
+    private Integer ingredientAmount;
+    private Integer ingredientPS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_amount_popup);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        ingredientName = intent.getStringExtra("ingredientName");
+        ingredientPS = Integer.parseInt(intent.getStringExtra("ingredientPS"));
+        ingredientAmount = Integer.parseInt(intent.getStringExtra("ingredientAmount"));
+
+        Log.i("name",ingredientName);
+        Log.i("ps",Integer.toString(ingredientPS));
+        Log.i("amoutn",Integer.toString(ingredientAmount));
+
+
+
+        ingreName.setText(ingredientName);
+        amountGram.setText(Integer.toString(ingredientAmount));
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                amountGram.setText(""+Integer.toString(Integer.parseInt(amountGram.getText().toString())-10));
+            }
+        });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                amountGram.setText(""+Integer.toString(Integer.parseInt(amountGram.getText().toString())+10));
+            }
+        });
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("setIngredient",Integer.toString(ingredientPS));
+
+                returnIntent.putExtra("setIngredientAmount",amountGram.getText().toString());
+                setResult(Activity.RESULT_OK,returnIntent);
+                finish();
+            }
+        });
+
+
+    }
+}
