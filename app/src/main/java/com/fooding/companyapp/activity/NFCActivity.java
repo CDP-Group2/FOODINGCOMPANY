5d75d8db10e7f0254ebfb338b0689ec701ded8b3
jaemyungskku <jaemyung@skku.edu>
2017-12-05 17:41:08 +0900

    NFC

--- app/src/main/java/com/fooding/companyapp/activity/NFCActivity.java
+++ app/src/main/java/com/fooding/companyapp/activity/NFCActivity.java
@@ -1,5 +1,6 @@
 package com.fooding.companyapp.activity;
 
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.nfc.NdefMessage;
@@ -9,19 +10,27 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.fooding.companyapp.APIService;
 import com.fooding.companyapp.FoodingCompanyApplication;
 import com.fooding.companyapp.R;
 import com.fooding.companyapp.data.Food;
+import com.fooding.companyapp.data.model.Ingredient;
 
 import java.nio.charset.Charset;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NFCActivity extends AppCompatActivity {
     @BindView(R.id.my_page)
@@ -115,27 +124,73 @@ public class NFCActivity extends AppCompatActivity {
         super.onNewIntent(intent);
         if (intent == null)
             return;
-        Tag mTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        if (mTag != null)
-            Toast.makeText(this,mTag.toString(),Toast.LENGTH_SHORT);
+        Parcelable msg[] = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (msg != null) {
+            Log.d("d", "hi");
+            String dMsg = setReadTagData((NdefMessage) msg[0]);
+            Toast.makeText(this, dMsg, Toast.LENGTH_SHORT).show();
+
+            Retrofit retrofit;
+            APIService apiService;
+
+            retrofit = new Retrofit.Builder().baseUrl(APIService.API_URL).addConverterFactory(GsonConverterFactory.create()).build();
+            apiService = retrofit.create(APIService.class);
+
+            Call<Ingredient> comment = apiService.getIngredientInfo(dMsg);
+            comment.enqueue(new Callback<Ingredient>() {
+                @Override
+                public void onResponse(Call<Ingredient> call, Response<Ingredient> response) {
+                    if(response.isSuccessful()) {
+                        // results = "";
+                        Log.i("foo","foo");
+                        Ingredient temp = response.body();
+                        String results = temp.getName();
+                        String resultsID = temp.getId();
+
+                        Intent returnIntent = new Intent();
+                        // returnIntent.putExtra("addIngredient","fromCamera : " +lastText);
+                        returnIntent.putExtra("addIngredient",results.toString());
+                        returnIntent.putExtra("addIngredientID",resultsID.toString());
+                        setResult(Activity.RESULT_OK,returnIntent);
+                        finish();
+                    } else {
+                        Log.i("Get Ingredient Info", "Fail");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Ingredient> call, Throwable t) {
+                    Log.i("Get Ingredient Info", "On Failure");
+                    t.printStackTrace();
+                }
+            });
+        }
     }
 
-    public void setReadTagData(NdefMessage ndefmsg) {
+    public String setReadTagData(NdefMessage ndefmsg) {
+        String strRec =null;
         if (ndefmsg == null) {
-            return;
+            return strRec;
         }
-        String msgs = "";
-        msgs += ndefmsg.toString() + "\n";
         NdefRecord[] records = ndefmsg.getRecords();
         for (NdefRecord rec : records) {
             byte[] payload = rec.getPayload();
-            String textEncoding = "UTF-8";
-            if (payload.length > 0)
-                textEncoding = (payload[0] & 0200) == 0 ? "UTF-8" : "UTF-16";
-
-            Short tnf = rec.getTnf();
-            String type = String.valueOf(rec.getType());
-            String payloadStr = new String(rec.getPayload(), Charset.forName(textEncoding));
+            // 버퍼 데이터를 인코딩 변환
+            strRec = byteDecoding(payload);
         }
+        return strRec;
+    }
+    public String byteDecoding(byte[] buf) {
+        String strText="";
+        String textEncoding = ((buf[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+        int langCodeLen = buf[0] & 0077;
+
+        try {
+            strText = new String(buf, langCodeLen + 1,
+                    buf.length - langCodeLen - 1, textEncoding);
+        } catch(Exception e) {
+            Log.d("tag1", e.toString());
+        }
+        return strText;
     }
 }
