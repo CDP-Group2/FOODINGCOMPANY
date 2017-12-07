fdae70b7cb86604ba708824281dfac978bae81d2
poerty <s2peters2@naver.com>
2017-12-07 17:40:00 +0900

    직접입력추가

---
+++ app/src/main/java/com/fooding/companyapp/activity/plusPopupActivity.java
@@ -0,0 +1,92 @@
+package com.fooding.companyapp.activity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fooding.companyapp.APIService;
+import com.fooding.companyapp.FoodingCompanyApplication;
+import com.fooding.companyapp.R;
+import com.fooding.companyapp.data.Food;
+import com.fooding.companyapp.data.model.Ingredient;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class plusPopupActivity extends Activity {
+    @BindView(R.id.okay) Button ok;
+    @BindView(R.id.NEWNAME) EditText ingredientN;
+
+    private String ingredientName;
+    private Integer ingredientAmount;
+    private Integer ingredientPS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_plus_popup);
+        ButterKnife.bind(this);
+
+        /*************************************************************************************************************/
+        // font setting
+        final FoodingCompanyApplication app = FoodingCompanyApplication.getInstance();
+        SharedPreferences myPref = app.getMyPref();
+
+        /*final String pathT = myPref.getString("titleFont", "none");
+        Typeface font = Typeface.createFromAsset(getAssets(), pathT);
+        title.setTypeface(font);*/
+
+        final String pathK = myPref.getString("koreanFont", "none");
+        Typeface fontK = Typeface.createFromAsset(getAssets(), pathK);
+        final String pathKB = myPref.getString("boldKoreanFont", "none");
+        Typeface fontKB = Typeface.createFromAsset(getAssets(), pathKB);
+
+        ingredientN.setTypeface(fontKB);
+        ok.setTypeface(fontK);
+        /*************************************************************************************************************/
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("Ingredient",ingredientN.getText().toString());
+
+                setResult(Activity.RESULT_OK,returnIntent);
+                finish();
+            }
+        });
+
+
+    }
+}
